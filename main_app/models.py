818a7bb5060e76c6972915cf05a071357cd98227
Julie Webb <julief.webb@gmail.com>
2022-07-11 19:24:58 -0400

    adds authentication and models

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse
+
+T_UNITS = (
+    ('s', 'Seconds'),
+    ('m', 'Minutes'),
+    ('h', 'Hours')
+)
+
 
 # Create your models here.
+class Post(models.Model):
+    time_int = models.IntegerField()
+    time_unit = models.CharField(
+        max_length=10,
+        choices = T_UNITS,
+        default=T_UNITS[0][0])
+    time_stamp = models.DateTimeField(auto_now_add=True)
+    desc = models.CharField(max_length=500)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class Photo_Pair(models.Model):
+    ref_url = models.CharField(max_length=250)
+    art_url = models.CharField(max_length=250)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
+class Post:
+    def __init__(self, timeint, timeunit, timestamp, description, user):
+        self.timeint = timeint,
+        self.timeunit = timeunit,
+        self.timestamp = timestamp,
+        self.description = description,
+        self.user = user
+
+posts = [
+    Post(3, 'minute', '12/12/2121 5:00pm', 'quick hands practice', 'jfwebb'),
+    Post(2, 'minute', '01/04/2006 4:00pm', 'eyes practice', 'msteele')
+]
+
+def signup (request):
+    error_message = ''
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            # automatically login created user
+            login(request, user)
+            return redirect('posts')
+    else:
+        error_message = 'Invalid Sign Up - Try Again'
+    form = UserCreationForm()
+    context = {'form': form, 'error_message': error_message}
+    return render(request, 'registration/signup.html', context)
+
+def home(request):
+    return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
+
+def about(request):
+    return render(request, 'about.html')
+
+def posts_index(request):
+    return HttpResponse('<h1>Posts Here /ᐠ｡‸｡ᐟ\ﾉ</h1>')
