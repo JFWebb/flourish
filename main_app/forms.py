0dbf61d015b93a8237fe45986273cdfb814f26e3
Julie Webb <julief.webb@gmail.com>
2022-07-13 23:48:28 -0400

    adds styling

--- main_app/forms.py
+++ main_app/forms.py
@@ -1,11 +1,12 @@
 from django.forms import ModelForm
+
 from .models import Post
 
 class Post_Form(ModelForm):
     class Meta:
         model = Post
         fields = ['time_int', 'time_unit', 'desc']
-        
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         for field in self.fields.values():
